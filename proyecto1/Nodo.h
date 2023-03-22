6e4afc47b68583fb275cf96e8995e34aed9bd51a
mmunozm9 <123638681+mmunozm9@users.noreply.github.com>
2023-03-21 20:24:42 -0500

    Add files via upload

---
+++ proyecto1/Nodo.h
@@ -0,0 +1,42 @@
+#include "Habitacion.h"
+#include <iostream>
+#ifndef PROYECTO1_NODO_H
+#define PROYECTO1_NODO_H
+
+
+class Nodo {
+protected:
+    string dato1;
+    int dato2;
+    Nodo *previo;
+    Nodo *sgte;
+    friend class Lista;
+
+
+public:
+    Nodo();
+
+    Nodo(string dato1,int dato2);
+
+    Nodo( string dato1, int dato2,Nodo *previo, Nodo *sgte);
+
+    Nodo *getPrevio() const;
+
+    void setPrevio(Nodo *previo);
+
+    Nodo *getSgte() const;
+
+    void setSgte(Nodo *sgte);
+
+    string getDato1() const;
+
+    void setDato1(string dato1);
+
+    int getDato2() const;
+
+    void setDato2(int dato2);
+
+};
+
+
+#endif //PROYECTO1_NODO_H
